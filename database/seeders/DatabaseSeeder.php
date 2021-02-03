42f398ad38ac619293b902ba006c5a4a529d3d57
tubagus1108 <tubagussananto2000@gmail.com>
2021-02-03 14:25:27 +0700

    tes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\History::factory(10)->create();
+        \App\Models\User::factory(1)->create();
     }
 }
