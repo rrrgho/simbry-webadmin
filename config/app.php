8bc6f1a21cdbbcde41700bee65fb1051f221ecfc
Muhammad Tubagus Sananto <tubagussananto2000@gmail.com>
2021-10-07 09:38:09 +0700

    change http to https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://admin.ypsimlibrary.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -229,7 +229,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
         'LogActivity' => App\Helpers\LogActivity::class,
-        
+
 
     ],
 
