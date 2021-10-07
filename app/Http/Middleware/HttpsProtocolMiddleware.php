c66c8c589b154c73807eec5da14dc5cd4af3b84a
Muhammad Tubagus Sananto <tubagussananto2000@gmail.com>
2021-10-07 10:34:44 +0700

    fix http to https

--- app/Http/Middleware/HttpsProtocolMiddleware.php
+++ app/Http/Middleware/HttpsProtocolMiddleware.php
@@ -16,7 +16,7 @@ class HttpsProtocolMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && app()->environment('production')) {
+        if (!$request->secure() && app()->environment('local')) {
             return redirect()->secure($request->getRequestUri());
         }
 
