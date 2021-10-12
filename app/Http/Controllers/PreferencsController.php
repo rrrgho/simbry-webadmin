e7e1aeab14fda6e58365aaa0db3fa0c2fbab614d
Muhammad Tubagus Sananto <tubagussananto2000@gmail.com>
2021-10-12 20:08:38 +0700

    fix

--- app/Http/Controllers/PreferencsController.php
+++ app/Http/Controllers/PreferencsController.php
@@ -85,7 +85,7 @@ class PreferencsController extends Controller
     {
         $data = Preferensi::where('user_id',Auth::guard('api')->user()->id)->where('deleted_at',null)->get();
         if($data == '[]')
-            return response()->json(['error' => true,'message' => 'Data not Found!!']);
+            return response()->json(['error' => true,'message' => 'Data not Found!!','data' => $data]);
         return response()->json(['error' => false,'message' => 'Success get data','data' => $data,'isStatus' => $data[0]->status == 1 ? true : false]);
     }
     public function preferensiDataTable()
